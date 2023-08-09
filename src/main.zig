6cfb6bbe6a2cd99309f2879c68327bf909246e8c
Richard Palethorpe <io@richiejp.com>
2023-08-09 17:15:17 +0100

    Implement switch_root in Zig based init to allow pivot_root
    pivot_root, used by nsjail and container runtimes, does not work on
    the initial rootfs. So we have to create a new FS and switch to it.
    
    So far the Zig based init just does that and mounts /proc & /sys. Then
    hands off to Busybox.

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,14 @@ const std = @import("std");
 const log = std.log;
 const info = std.log.info;
 const fs = std.fs;
-const linux = std.os.linux;
+const os = std.os;
+const errno = os.errno;
+const linux = os.linux;
 const E = linux.E;
+const mem = std.mem;
+const Allocator = mem.Allocator;
+
+const MS_MOVE = 8192;
 
 fn mount(
     special: [*:0]const u8,
@@ -12,8 +18,8 @@ fn mount(
     flags: u32,
     data: usize,
 ) !void {
-    const ret: E =
-        @enumFromInt(linux.mount(special, dir, fstype, flags, data));
+    const ret =
+        errno(linux.mount(special, dir, fstype, flags, data));
 
     if (ret == E.SUCCESS)
         return;
@@ -23,7 +29,17 @@ fn mount(
     return error.mountFailed;
 }
 
-fn ls(dir: fs.Dir, path: []const u8) !void {
+fn chroot(path: [*:0]const u8) !void {
+    const ret = errno(linux.chroot(path));
+
+    if (ret == E.SUCCESS)
+        return;
+
+    return error.chrootFailed;
+}
+
+fn ls(path: []const u8) !void {
+    var dir = try fs.openDirAbsolute(path, .{});
     var idir = try dir.openIterableDir(path, .{});
     defer idir.close();
     var itr = idir.iterate();
@@ -42,26 +58,153 @@ fn ls(dir: fs.Dir, path: []const u8) !void {
     }
 }
 
-fn chmod(dir: fs.Dir, path: []const u8, mode: fs.File.Mode) !void {
-    var sys = try dir.makeOpenPathIterable(path, .{});
-    defer sys.close();
+fn fstatat(dir: fs.Dir, path: [*:0]const u8, stat_buf: *linux.Stat) !void {
+    const ret = errno(linux.fstatat(dir.fd, path, stat_buf, 0));
 
-    try sys.chmod(mode);
+    if (ret == E.SUCCESS)
+        return;
+
+    return error.fstatatFailed;
+}
+
+fn mvRec(src: fs.Dir, dst: fs.Dir, root_dev: linux.dev_t) !void {
+    var isrc = try src.openIterableDir(".", .{});
+    defer isrc.close();
+    var itr = isrc.iterateAssumeFirstIteration();
+
+    const dst_stat = try os.fstat(dst.fd);
+
+    while (try itr.next()) |base| {
+        switch (base.kind) {
+            .file => {
+                var src_stat: linux.Stat = undefined;
+
+                try fstatat(src, &try os.toPosixPath(base.name), &src_stat);
+
+                if (src_stat.dev != root_dev) {
+                    info("skip file {s}", .{base.name});
+                    continue;
+                }
+
+                info("mv file {s}", .{base.name});
+
+                try src.copyFile(base.name, dst, base.name, .{});
+                try src.deleteFile(base.name);
+            },
+            .sym_link => {
+                info("ln {s}", .{base.name});
+                var buf: [linux.PATH_MAX]u8 = undefined;
+
+                const link = try src.readLink(base.name, &buf);
+                try dst.symLink(link, base.name, .{});
+                try src.deleteFile(base.name);
+            },
+            .directory => {
+                var sub_src = try src.openDir(base.name, .{});
+                defer sub_src.close();
+                const sub_src_stat = try os.fstat(sub_src.fd);
+
+                info("stat dir {s}: {}", .{ base.name, sub_src_stat });
+
+                if (sub_src_stat.ino == dst_stat.ino) {
+                    info("skip dir {s}", .{base.name});
+                    continue;
+                }
+
+                var sub_dst = try mkpath(dst, base.name, 0o755);
+                defer sub_dst.close();
+
+                if (root_dev != sub_src_stat.dev) {
+                    info("shallow mv {s}", .{base.name});
+                    continue;
+                }
+
+                info("mv dir {s}", .{base.name});
+                try mvRec(sub_src, sub_dst, root_dev);
+
+                src.deleteDir(base.name) catch |err| {
+                    switch (err) {
+                        error.DirNotEmpty => {
+                            info("left {s}", .{base.name});
+                        },
+                        else => return err,
+                    }
+                };
+            },
+            else => {
+                info("skip {} {s}", .{ base.kind, base.name });
+            },
+        }
+    }
+}
+
+fn mkpath(dir: fs.Dir, path: []const u8, mode: fs.File.Mode) !fs.Dir {
+    var new = try dir.makeOpenPath(path, .{});
+    errdefer new.close();
+
+    var inew = try dir.openIterableDir(".", .{});
+    defer inew.close();
+
+    try inew.chmod(mode);
+
+    return new;
+}
+
+fn switch_root() !void {
+    var old_root = try fs.openDirAbsolute("/", .{});
+    defer old_root.close();
+    const root_dev = (try os.fstat(old_root.fd)).dev;
+
+    {
+        var new_root = try mkpath(old_root, "newroot", 0o700);
+        defer new_root.close();
+        const data: [*:0]const u8 = "size=100240k";
+        try mount("none", "/newroot", "tmpfs", 0, @intFromPtr(data));
+        info("Temporarily mounted new root at /newroot", .{});
+    }
+
+    var new_root = try old_root.openDir("newroot", .{});
+    defer new_root.close();
+    try mvRec(old_root, new_root, root_dev);
+    info("Moved root contents to /newroot", .{});
+
+    try std.os.chdir("/newroot");
+    try mount(".", "/", ":-)", MS_MOVE, 0);
+    info("Moved /newroot to /", .{});
+
+    try chroot(".");
+    try std.os.chdir("/");
+    info("chrooted into new /", .{});
 }
 
 fn init() !void {
-    var root = try fs.openDirAbsolute("/", .{});
-    defer root.close();
+    info("Switching root away from rootfs because of pivot_root", .{});
+    try switch_root();
 
-    try chmod(root, "sys", 0o555);
+    info("Mounting /proc and /sys, we'll get proper error traces now", .{});
+    try mount("none", "/proc", "proc", 0, 0);
     try mount("none", "/sys", "sysfs", 0, 0);
 
-    try ls(root, ".");
-    try ls(root, "dev");
-    try ls(root, "sys");
+    //The Rest is left busybox sh and /etc/init.d/rcS for now
+}
+
+fn rcS(allc: Allocator) !void {
+    var child = std.process.Child.init(&[_][]const u8{
+        "/bin/sh",
+        "/etc/init.d/rcS",
+    }, allc);
+
+    _ = try child.spawnAndWait();
+}
+
+fn sh(allc: Allocator) !void {
+    var child = std.process.Child.init(&[_][]const u8{"/bin/sh"}, allc);
+
+    _ = try child.spawnAndWait();
 }
 
 pub fn main() !void {
+    const allc = std.heap.page_allocator;
     const is_init = std.os.linux.getpid() == 1;
 
     if (is_init) {
@@ -73,16 +216,9 @@ pub fn main() !void {
     info("uname: {s} {s} {s} {s} {s} {s}", std.os.uname());
 
     if (is_init) {
-        init() catch |err| {
-            if (@errorReturnTrace()) |bt| {
-                log.err("during setup: {}: {}", .{ err, bt });
-            } else {
-                log.err("during setup: {}", .{err});
-            }
-        };
+        try init();
+        try rcS(allc);
     }
 
-    while (is_init) {
-        std.time.sleep(100000);
-    }
+    try sh(allc);
 }
