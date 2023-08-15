365da5eaee231bde615ea89c93ac142e46296d4d
Richard Palethorpe <io@richiejp.com>
2023-08-15 14:23:52 +0100

    Allow more qemu args to be passed

--- script/run-qemu.sh
+++ script/run-qemu.sh
@@ -4,6 +4,8 @@ arch=$1
 initrd=$2
 kernel=$3
 
+shift 3
+
 Q=qemu-system
 
 case $arch in
@@ -17,7 +19,8 @@ case $arch in
                            -kernel $kernel \
                            -initrd $initrd \
                            -serial stdio \
-                           -append 'console=ttyAMA0 earlyprintk=ttyAMA0';;
+                           -append 'console=ttyAMA0 earlyprintk=ttyAMA0' \
+                           $@;;
         kvm) \
                 qemu-kvm -m 1G \
                          -smp 2 \
@@ -28,7 +31,8 @@ case $arch in
                          -kernel $kernel \
                          -initrd $initrd \
                          -serial stdio \
-                         -append 'console=ttyS0 earlyprintk=ttyS0';;
+                         -append 'console=ttyS0 earlyprintk=ttyS0' \
+                         $@;;
         *) echo "Don't recognise $arch"
            exit 1;;
 esac
