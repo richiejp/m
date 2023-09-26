85ece3a81fde4cfe9d5dfebdae0bacb6da2892ef
Richard Palethorpe <io@richiejp.com>
2023-09-26 13:24:50 +0100

    Add 9P to qemu params

--- script/run-qemu.sh
+++ script/run-qemu.sh
@@ -10,8 +10,8 @@ Q=qemu-system
 
 case $arch in
         arm64|aarch64) \
-                $Q-aarch64 -m 1G \
-                           -smp 2 \
+                $Q-aarch64 -m 4G \
+                           -smp 4 \
                            -display none \
                            -machine virt -cpu cortex-a57 \
 		           -nodefaults \
@@ -22,8 +22,8 @@ case $arch in
                            -append 'console=ttyAMA0 earlyprintk=ttyAMA0' \
                            $@;;
         kvm) \
-                qemu-kvm -m 1G \
-                         -smp 2 \
+                qemu-kvm -m 4G \
+                         -smp 4 \
                          -cpu host \
                          -display none \
 		         -nodefaults \
@@ -31,8 +31,12 @@ case $arch in
                          -kernel $kernel \
                          -initrd $initrd \
                          -serial stdio \
-                         -append 'console=ttyS0 earlyprintk=ttyS0' \
+                         -fsdev local,security_model=mapped,id=fsdev-fs0,path=$(realpath .) \
+                         -device virtio-9p-pci,fsdev=fsdev-fs0,mount_tag=fs0 \
+                         -append 'console=ttyS0 earlyprintk=ttyS0 nokaslr panic=0' \
                          $@;;
         *) echo "Don't recognise $arch"
            exit 1;;
 esac
+
+# slub_debug=T,kmalloc-512
